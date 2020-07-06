a14530351e135e2b5eb8e54466408bdef93d1919
TattlingJam4231 <41134508+TattlingJam4231@users.noreply.github.com>
2020-07-06 05:20:05 -0700

    Update localizationmanager.lua

--- loc/localizationmanager.lua
+++ loc/localizationmanager.lua
@@ -1,7 +1,7 @@
 Hooks:Add("LocalizationManagerPostInit", "Armorer Buff Localization", function(loc)
 	LocalizationManager:add_localized_strings({
-        ["menu_deck3_3_desc"] = "You gain an additional ##10%## more armor.\n\nDamage you take beyond ##50## is reduced by ##30%##.",
-        ["menu_deck3_5_desc"] = "You gain an additional ##10%## more armor.\n\nDamage you take beyond ##100## is reduced by an additional ##30%##.",
-        ["menu_deck3_9_desc"] = "You gain an additional ##5%## more armor.\n\nReduces the armor recovery time for you and your crew by ##10%##.\n\nDamage you take beyond ##150## is reduced by an additional ##30%##.\n\nDeck Completion Bonus: Your chance of getting a higher quality item during a PAYDAY is increased by ##10%##."
+        ["menu_deck3_3_desc"] = "You gain an additional ##10%## more armor.\n\nDamage you take exceeding ##50## is reduced by ##30%##.",
+        ["menu_deck3_5_desc"] = "You gain an additional ##10%## more armor.\n\nDamage you take exceeding ##100## is reduced by an additional ##30%##.",
+        ["menu_deck3_9_desc"] = "You gain an additional ##5%## more armor.\n\nReduces the armor recovery time for you and your crew by ##10%##.\n\nDamage you take exceeding ##150## is reduced by an additional ##30%##.\n\nDeck Completion Bonus: Your chance of getting a higher quality item during a PAYDAY is increased by ##10%##."
     })
 end)
